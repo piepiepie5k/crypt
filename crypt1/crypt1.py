62fdafd88c729eebf3b792f39eae20278a826a6e
piepiepie5k <egorc18@gmail.com>
2024-05-31 01:34:54 +0700

    Update crypt1.py

--- crypt1/crypt1.py
+++ crypt1/crypt1.py
@@ -1,5 +1,48 @@
 from caesarcipher import CaesarCipher
 import random
+
+
+
+def caesar_cipher(text, shift):
+    result = ""
+    
+    for char in text:
+        if char.isupper():
+            pos = ord(char) - ord('A')
+            pos = (pos + shift) % 26
+            result += chr(pos + ord('A'))
+        elif char.islower():
+            pos = ord(char) - ord('a')
+            pos = (pos + shift) % 26
+            result += chr(pos + ord('a'))
+        else:
+            result += char
+    
+    return result
+
+
+
+
+def caesar_decipher(text, shift):
+    result = ""
+    
+    for char in text:
+        if char.isupper():
+            pos = ord(char) - ord('A')
+            pos = (pos - shift) % 26
+            result += chr(pos + ord('A'))
+        elif char.islower():
+            pos = ord(char) - ord('a')
+            pos = (pos - shift) % 26
+            result += chr(pos + ord('a'))
+        else:
+
+            result += char
+    
+    return result
+
+
+
 source='I want to encode this string'
 print("before caesar")
 print(source)
@@ -7,20 +50,20 @@ print("enter key")
 key=random.randint(0,25)
 print("key: ", key)
 print("encrypt")
-cipher = CaesarCipher('I want to encode this string', offset=key)
-string=cipher.encoded
-print(cipher.encoded)
+cipher = caesar_cipher('I want to encode this string',key)
+string=cipher
+print(cipher)
 print("decrypt")
-cipher = CaesarCipher(string, offset=key)
-print(cipher.decoded)
+cipher = caesar_decipher(string, key)
+print(cipher)
 
 
 break_key=0
 print("breaking key using cipher text and open text")
-print("open text: ", source, "cipher text: ", cipher.encoded)
+print("open text: ", source, "cipher text: ", string)
 while(break_key<26):
-    cipher=CaesarCipher(source, offset=break_key)
-    if(cipher.encoded==string):
+    cipher=caesar_decipher(source, break_key)
+    if(cipher==string):
         break
     break_key+=1
 
@@ -29,8 +72,8 @@ print("key is: ",break_key)
 print("breaking using only cipher\n cipher text: ", string)
 break_key=0
 while(break_key<26):
-    cipher=CaesarCipher(string, offset=break_key)
-    print("cipher text: ", string, "decoded: ", cipher.encoded, "key: ", break_key)
+    cipher=caesar_decipher(string, break_key)
+    print("cipher text: ", string, "decoded: ", cipher, "key: ", break_key)
     break_key+=1
 
 flag=0
@@ -43,8 +86,8 @@ number_words=len(words)
 print("breaking using cipher and dictionary\n cipher text: ", string)
 break_key=0
 while(break_key<26):
-    cipher=CaesarCipher(string, offset=break_key)
-    new_text=cipher.encoded
+    cipher=caesar_decipher(string, break_key)
+    new_text=cipher
     print("cipher text: ", string, "decoded: ", new_text, "key: ", break_key)
     words = new_text.split()
     for i in range(number_words):
